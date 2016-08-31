cce870aaf4246b3abf4b356e4003710d9c939ef7
zzdeagle545zz <deagles545@gmail.com>
2016-08-31 09:31:03 -0700

    Signed-off-by: zzdeagle545zz <deagles545@gmail.com>

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+    CIS-12
+    Christopher Garcia
+    Lecture Website
+    Updated on:8:30 8/31/16
+-->
+
+
+<html>
+<!-- Assigns the title and Links CSS StyleSheet -->
+    <head>
+        <meta charset="utf-8">
+        <title>LectureWebsite</title>
+        <link rel="stylesheet" href="main.css">
+    </head>
+<!-- Fill the Body with Notes and Functionality -->
+    <body>
+        <h1>
+            Chapter One of PHP
+        </h1>
+            <p>
+                The PHP tag is <?php     ?> and comments in the the php tag are
+                hidden to viewers. The following is printed by PHP!
+                <?php 
+                //Can you see me?
+                    echo 'Hello World' 
+                ?> 
+            </p>
+        <h2>
+            Chapter One of Java Scrpit
+        </h2>
+            <p>
+                
+            </p>
+        
+        
+        
+        
+        
+    
+    </body>
+
+
+
+
+</html>
